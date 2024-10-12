244524025c6c8b614493dc3e62668abdcb834b9e
sannu singh <sannusingh1509@gmail.com>
2024-10-12 14:15:18 +0530

    Initial commit

---
+++ customer-ticketing-system/src/main/java/com/example/customer_ticketing_system/CustomerTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_ticketing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ customer-ticketing-system/src/test/java/com/example/customer_ticketing_system/CustomerTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customer_ticketing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
